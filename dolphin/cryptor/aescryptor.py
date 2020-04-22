1ca4f69a49bbdfdd3cdd4eef060306fa841639c6
ThisIsClark <liuyuchibubao@gmail.com>
2020-04-22 17:34:59 +0800

    Format improvement

--- dolphin/cryptor/aescryptor.py
+++ dolphin/cryptor/aescryptor.py
@@ -11,8 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from Crypto.Cipher import AES
 from binascii import b2a_hex, a2b_hex
+
 from dolphin.cryptor import basecryptor
 
 
