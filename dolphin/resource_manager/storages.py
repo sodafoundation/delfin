e860309aa1c9d289a288a1bf2e949c4a485b4fba
root <pravin ranjan>
2020-04-23 11:49:36 +0530

    confct resolve

--- dolphin/resource_manager/storages.py
+++ dolphin/resource_manager/storages.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""The storages api implementation."""
+"""The storages api implementation"""
 
 import copy
 
