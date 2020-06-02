e3ff20c698e80f5c5060fdb3b2a61a8d580f583e
ThisIsClark <liuyuchibubao@gmail.com>
2020-06-02 19:02:47 +0800

    Add test_sync

--- dolphin/tests/unit/api/v1/test_storages.py
+++ dolphin/tests/unit/api/v1/test_storages.py
@@ -36,8 +36,7 @@ class TestStorageController:
         sc = StorageController()
         req = Request()
 
-        # Get storage failed, raise exception,
-        # do not call remove_storage_resource
+        # Get storage failed, expect to get exc.HTTPBadRequest
         mocker.patch('dolphin.db.storage_get',
                      side_effect=exception.StorageNotFound(
                          id='83df8a62-9ae4-4ffc-9948-5524cc7cdd64'))
@@ -46,7 +45,7 @@ class TestStorageController:
         mocker.patch('dolphin.db.storage_get')
 
         # Get any one of task lock failed
-        # do not call remove_storage_resource
+        # Expect to get tooz_coordination.LockAcquireFailed
         mocker.patch('dolphin.coordination.Lock.__enter__',
                      side_effect=tooz_coordination.LockAcquireFailed(
                          'can not acquire the lock'))
@@ -56,10 +55,49 @@ class TestStorageController:
 
         # Get storage successfully, call remove_storage_resource
         # Call count depends on the StorageResourceTask's subclasses' count
-        mock_remove_storage_resource.reset_mock()
         sc.delete(req, '83df8a62-9ae4-4ffc-9948-5524cc7cdd64')
         expected_count = 0
         for _ in task.StorageResourceTask.__subclasses__():
             expected_count += 1
         assert expected_count == mock_remove_storage_resource.call_count
         assert 1 == mock_remove_storage_in_cache.call_count
+
+    def test_sync(self, mocker):
+        # For StorageController.__init__
+        mocker.patch('dolphin.task_manager.rpcapi.TaskAPI.__init__',
+                     return_value=None)
+        # For StorageController.sync
+        mock_sync_storage_resource = mocker.patch(
+            'dolphin.task_manager.rpcapi.TaskAPI.sync_storage_resource')
+        mocker.patch(
+            'dolphin.coordination.Lock.__init__', return_value=None)
+        mocker.patch('dolphin.coordination.Lock.__enter__')
+        mocker.patch('dolphin.coordination.Lock.__exit__')
+
+        sc = StorageController()
+        req = Request()
+
+        # Get storage failed, expect to get exc.HTTPBadRequest
+        mocker.patch('dolphin.db.storage_get',
+                     side_effect=exception.StorageNotFound(
+                         id='83df8a62-9ae4-4ffc-9948-5524cc7cdd64'))
+        with pytest.raises(exc.HTTPNotFound):
+            sc.sync(req, '83df8a62-9ae4-4ffc-9948-5524cc7cdd64')
+        mocker.patch('dolphin.db.storage_get')
+
+        # Get any one of task lock failed
+        # Expect to get tooz_coordination.LockAcquireFailed
+        mocker.patch('dolphin.coordination.Lock.__enter__',
+                     side_effect=tooz_coordination.LockAcquireFailed(
+                         'can not acquire the lock'))
+        with pytest.raises(exc.HTTPBadRequest):
+            sc.sync(req, '83df8a62-9ae4-4ffc-9948-5524cc7cdd64')
+        mocker.patch('dolphin.coordination.Lock.__enter__')
+
+        # Get storage successfully, call sync_storage_resource
+        # Call count depends on the StorageResourceTask's subclasses' count
+        sc.sync(req, '83df8a62-9ae4-4ffc-9948-5524cc7cdd64')
+        expected_count = 0
+        for _ in task.StorageResourceTask.__subclasses__():
+            expected_count += 1
+        assert expected_count == mock_sync_storage_resource.call_count
