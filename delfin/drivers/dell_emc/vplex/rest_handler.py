7f242eb75550ad7a63d366508f4c58a8cfa94bb7
liuxh <xiaohuan.liu@gh-ca.com>
2021-03-18 17:12:45 +0800

    vplex代码合并

---
+++ delfin/drivers/dell_emc/vplex/rest_handler.py
@@ -0,0 +1,126 @@
+# Copyright 2021 The SODA Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import six
+from oslo_log import log as logging
+
+from delfin import cryptor
+from delfin import exception
+from delfin.drivers.dell_emc.vplex import consts
+from delfin.drivers.utils.rest_client import RestClient
+
+LOG = logging.getLogger(__name__)
+
+
+class RestHandler(RestClient):
+
+    def __init__(self, **kwargs):
+        super(RestHandler, self).__init__(**kwargs)
+
+    def login(self):
+        try:
+            data = {}
+            self.init_http_head()
+            self.session.headers.update({
+                "username": self.rest_username,
+                "password": cryptor.decode(self.rest_password)})
+            res = self.do_call(consts.REST_AUTH_URL, data, 'GET')
+            if res.status_code != 200:
+                LOG.error("Login error. URL: %(url)s\n"
+                          "Reason: %(reason)s.",
+                          {"url": consts.REST_AUTH_URL, "reason": res.text})
+                if 'User authentication failed' in res.text:
+                    raise exception.InvalidUsernameOrPassword()
+                else:
+                    raise exception.BadResponse(res.text)
+        except Exception as e:
+            LOG.error("Login error: %s", six.text_type(e))
+            raise e
+
+    def get_rest_info(self, url, data=None, method='GET'):
+        """Return dict result of the url response."""
+        result_json = None
+        res = self.do_call(url, data, method)
+        if res.status_code == 200:
+            result_json = res.json().get('response')
+        return result_json
+
+    def get_virtual_volume_by_name_resp(self, cluster_name,
+                                        virtual_volume_name):
+        url = '%s/clusters/%s/virtual-volumes/%s' % \
+              (consts.BASE_CONTEXT, cluster_name, virtual_volume_name)
+        response = self.get_rest_info(url)
+        return response
+
+    def get_virtual_volume_resp(self, cluster_name):
+        url = '%s/clusters/%s/virtual-volumes' % (
+            consts.BASE_CONTEXT, cluster_name)
+        response = self.get_rest_info(url)
+        return response
+
+    def get_cluster_resp(self):
+        uri = '%s/clusters' % consts.BASE_CONTEXT
+        response = self.get_rest_info(uri)
+        return response
+
+    def get_devcie_resp(self, cluster_name):
+        url = '%s/clusters/%s/devices' % (consts.BASE_CONTEXT, cluster_name)
+        response = self.get_rest_info(url)
+        return response
+
+    def get_device_by_name_resp(self, cluster_name, device_name):
+        url = '%s/clusters/%s/devices/%s' % (
+            consts.BASE_CONTEXT, cluster_name, device_name)
+        response = self.get_rest_info(url)
+        return response
+
+    def get_health_check_resp(self):
+        url = '%s/health-check' % consts.BASE_CONTEXT
+        data = {"args": "-l"}
+        response = self.get_rest_info(url, data, method='POST')
+        return response
+
+    def get_cluster_by_name_resp(self, cluster_name):
+        url = '%s/clusters/%s' % (consts.BASE_CONTEXT, cluster_name)
+        response = self.get_rest_info(url)
+        return response
+
+    def get_storage_volume_summary_resp(self, cluster_name):
+        url = '%s/storage-volume+summary' % consts.BASE_CONTEXT
+        args = '--clusters %s' % cluster_name
+        data = {"args": args}
+        response = self.get_rest_info(url, data, method='POST')
+        return response
+
+    def get_device_summary_resp(self, cluster_name):
+        url = '%s/local-device+summary' % consts.BASE_CONTEXT
+        args = '--clusters %s' % cluster_name
+        data = {"args": args}
+        response = self.get_rest_info(url, data, method='POST')
+        return response
+
+    def get_virtual_volume_summary_resp(self, cluster_name):
+        url = '%s/virtual-volume+summary' % consts.BASE_CONTEXT
+        args = '--clusters %s' % cluster_name
+        data = {"args": args}
+        response = self.get_rest_info(url, data, method='POST')
+        return response
+
+    def logout(self):
+        try:
+            if self.session:
+                self.session.close()
+        except Exception as e:
+            err_msg = "Logout error: %s" % (six.text_type(e))
+            LOG.error(err_msg)
+            raise e
---
+++ delfin/drivers/dell_emc/vplex/vplex_stor.py
@@ -0,0 +1,334 @@
+# Copyright 2021 The SODA Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import re
+
+import six
+from delfin import exception
+from oslo_log import log
+from oslo_utils import units
+
+from delfin.common import constants
+from delfin.drivers import driver
+from delfin.drivers.dell_emc.vplex import alert_handler
+from delfin.drivers.dell_emc.vplex import rest_handler
+
+LOG = log.getLogger(__name__)
+
+
+class VplexStorageDriver(driver.StorageDriver):
+    """DELL EMC VPLEX storage driver implement the DELL EMC Storage driver"""
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.rest_handler = rest_handler.RestHandler(**kwargs)
+        self.rest_handler.login()
+
+    def reset_connection(self, context, **kwargs):
+        self.rest_handler.logout()
+        self.rest_handler.verify = kwargs.get('verify', False)
+        self.rest_handler.login()
+
+    def get_storage(self, context):
+        health_check = self.rest_handler.get_health_check_resp()
+        all_cluster = self.rest_handler.get_cluster_resp()
+        cluster_name_list = VplexStorageDriver.get_resource_names(all_cluster)
+        if cluster_name_list:
+            health_map = {}
+            custom_data = health_check.get("custom-data")
+            VplexStorageDriver.handle_detail(custom_data,
+                                             health_map, split=':')
+            for cluster_name in cluster_name_list:
+                response = self.rest_handler.get_cluster_by_name_resp(
+                    cluster_name)
+                attr_map = VplexStorageDriver.get_attribute_map(response)
+                operate_status = attr_map.get('operational-status')
+                health_status = attr_map.get('health-state')
+                status = VplexStorageDriver.analyse_status(operate_status,
+                                                           health_status)
+                try:
+                    raw_capacity = self.get_cluster_raw_capacity(cluster_name)
+                    total_capacity = self.get_cluster_total_capacity(
+                        cluster_name)
+                    used_capacity = self.get_cluster_used_capacity(
+                        cluster_name)
+                except Exception:
+                    error_msg = "Failed to get capacity from VPLEX!"
+                    raise exception.StorageBackendException(error_msg)
+                free_capacity = total_capacity - used_capacity
+                if free_capacity < 0:
+                    free_capacity = 0
+                cluster = {
+                    'name': cluster_name,
+                    'vendor': 'DELL EMC',
+                    'description': 'EMC VPlex Storage',
+                    'status': status,
+                    'serial_number': attr_map.get('top-level-assembly'),
+                    'firmware_version': health_map.get("Product Version"),
+                    'model': 'EMC VPLEX ' + health_map.get("Product Type"),
+                    'location': '',
+                    'raw_capacity': int(raw_capacity),
+                    'total_capacity': int(total_capacity),
+                    'used_capacity': int(used_capacity),
+                    'free_capacity': int(free_capacity),
+                    'subscribed_capacity': 0
+                }
+                break
+        return cluster
+
+    def list_storage_pools(self, context):
+        device_list = []
+        all_cluster = self.rest_handler.get_cluster_resp()
+        cluster_name_list = VplexStorageDriver.get_resource_names(all_cluster)
+        for cluster_name in cluster_name_list:
+            response_device = self.rest_handler.get_devcie_resp(cluster_name)
+            map_device_childer = VplexStorageDriver.get_children_map(
+                response_device)
+            for name, resource_type in map_device_childer.items():
+                response_dn = self.rest_handler.get_device_by_name_resp(
+                    cluster_name, name)
+                map_dn_attribute = VplexStorageDriver.get_attribute_map(
+                    response_dn)
+                virtual_volume = map_dn_attribute.get("virtual-volume")
+                total_capacity_str = map_dn_attribute.get("capacity")
+                total_capacity = VplexStorageDriver.analyse_capacity(
+                    total_capacity_str)
+                operate_status = map_dn_attribute.get('operational-status')
+                health_status = map_dn_attribute.get('health-state')
+                used_capacity = 0
+                free_capacity = 0
+                if virtual_volume:
+                    used_capacity = total_capacity
+                else:
+                    free_capacity = total_capacity
+
+                device = {
+                    'name': name,
+                    'storage_id': self.storage_id,
+                    'native_storage_pool_id': map_dn_attribute.get(
+                        "system-id"),
+                    'description': 'EMC VPlex Pool',
+                    'status': self.analyse_status(operate_status,
+                                                  health_status),
+                    'storage_type': constants.StorageType.BLOCK,
+                    'total_capacity': int(total_capacity),
+                    'used_capacity': int(used_capacity),
+                    'free_capacity': int(free_capacity)
+                }
+                device_list.append(device)
+        return device_list
+
+    def list_volumes(self, context):
+        vv_list = []
+        all_cluster = self.rest_handler.get_cluster_resp()
+        cluster_name_list = VplexStorageDriver.get_resource_names(all_cluster)
+        for cluster_name in cluster_name_list:
+            resposne_vv = self.rest_handler.get_virtual_volume_resp(
+                cluster_name)
+            map_vv_children = VplexStorageDriver.get_children_map(resposne_vv)
+            for name, resource_type in map_vv_children.items():
+                response_vvn = self.rest_handler. \
+                    get_virtual_volume_by_name_resp(cluster_name, name)
+                map_vvn_attribute = VplexStorageDriver.get_attribute_map(
+                    response_vvn)
+                thin_enabled = map_vvn_attribute.get("thin-enabled")
+                operate_status = map_vvn_attribute.get('operational-status')
+                health_status = map_vvn_attribute.get('health-state')
+                vv_type = self.analyse_vv_type(thin_enabled)
+                total_capacity = VplexStorageDriver.analyse_capacity(
+                    map_vvn_attribute.get("capacity"))
+                vpd_id = map_vvn_attribute.get("vpd-id")
+                cells = vpd_id.split(":")
+                wwn = ''
+                if len(cells) > 1:
+                    wwn = cells[1]
+                used_capacity = 0
+                if vv_type == constants.VolumeType.THICK:
+                    used_capacity = total_capacity
+                vv = {
+                    'name': name,
+                    'storage_id': self.storage_id,
+                    'description': 'EMC VPlex volume',
+                    'status': self.analyse_status(operate_status,
+                                                  health_status),
+                    'native_volume_id': vpd_id,
+                    'native_storage_pool_id': map_vvn_attribute.get(
+                        'supporting-device'),
+                    'type': vv_type,
+                    'total_capacity': int(total_capacity),
+                    'used_capacity': int(used_capacity),
+                    'free_capacity': 0,
+                    'wwn': wwn
+                }
+                vv_list.append(vv)
+        return vv_list
+
+    def add_trap_config(self, context, trap_config):
+        pass
+
+    def remove_trap_config(self, context, trap_config):
+        pass
+
+    @staticmethod
+    def parse_alert(context, alert):
+        return alert_handler.AlertHandler().parse_alert(context, alert)
+
+    def list_alerts(self, context, query_para=None):
+        pass
+
+    def clear_alert(self, context, alert):
+        pass
+
+    @staticmethod
+    def get_access_url():
+        return 'https://{ip}'
+
+    @staticmethod
+    def get_attribute_map(response):
+        attr_map = {}
+        if response:
+            contexts = response.get("context")
+            for context in contexts:
+                attributes = context.get("attributes")
+                for attribute in attributes:
+                    key = attribute.get("name")
+                    value = attribute.get("value")
+                    attr_map[key] = value
+        return attr_map
+
+    @staticmethod
+    def analyse_capacity(capacity_str):
+        capacity = 0
+        if capacity_str.strip():
+            capacity = re.findall("\\d+", capacity_str)[0]
+        return capacity
+
+    @staticmethod
+    def analyse_status(operational_status, health_status):
+        status = constants.StorageStatus.ABNORMAL
+        status_normal = ["ok"]
+        status_offline = ["unknown", "isolated", "not-running",
+                          "non-recoverable-error"]
+        if operational_status and health_status in status_normal:
+            status = constants.StorageStatus.NORMAL
+        elif operational_status and health_status in status_offline:
+            status = constants.StorageStatus.OFFLINE
+        return status
+
+    @staticmethod
+    def analyse_vv_type(thin_enabled):
+        rs_type = constants.VolumeType.THICK
+        if thin_enabled == "enabled":
+            rs_type = constants.VolumeType.THIN
+        return rs_type
+
+    @staticmethod
+    def get_children_map(response):
+        child_map = {}
+        if response:
+            contexts = response.get("context")
+            for context in contexts:
+                childrens = context.get("children")
+                for children in childrens:
+                    name = children.get("name")
+                    type = children.get("type")
+                    child_map[name] = type
+        return child_map
+
+    @staticmethod
+    def get_resource_names(response):
+        resource_name_list = []
+        if response:
+            contexts = response.get('context')
+            for context in contexts:
+                childer_clusters = context.get("children")
+                for childer_cluster in childer_clusters:
+                    cluster_name = childer_cluster.get("name")
+                    resource_name_list.append(cluster_name)
+        return resource_name_list
+
+    @staticmethod
+    def handle_detail(detail_info, detail_map, split):
+        detail_arr = detail_info.split('\n')
+        for detail in detail_arr:
+            if detail is not None and detail != '':
+                strinfo = detail.split(split, 1)
+                key = strinfo[0]
+                value = ''
+                if len(strinfo) > 1:
+                    value = strinfo[1]
+                detail_map[key] = value
+
+    def get_cluster_raw_capacity(self, cluster_name):
+        resposne_summary = self.rest_handler. \
+            get_storage_volume_summary_resp(cluster_name)
+        try:
+            custom_data = resposne_summary.get("custom-data")
+            find_capacity = re.findall(
+                r"Capacity\s+total\s+(([0-9]*(\.[0-9]{1,3}))|([0-9]+))",
+                custom_data)
+            find_capacity_str = find_capacity[-1][0]
+            find_capacity_float = float(find_capacity_str)
+            capacity = int(find_capacity_float * units.Ti)
+        except Exception as e:
+            LOG.error("Storage raw capacity, cluster %s analyse error %s" %
+                      cluster_name, six.text_type(e))
+            raise e
+        return capacity
+
+    def get_cluster_total_capacity(self, cluster_name):
+        resposne_summary = self.rest_handler.get_device_summary_resp(
+            cluster_name)
+        try:
+            custom_data = resposne_summary.get("custom-data")
+            find_capacity = re.findall(
+                r'total.*?(([0-9]*(\.[0-9]{1,3}))|([0-9]+))',
+                custom_data)
+            find_capacity_str = find_capacity[-1][0]
+            find_capacity_float = float(find_capacity_str)
+            capacity = int(find_capacity_float * units.Ti)
+        except Exception as e:
+            LOG.error("Storage total capacity, cluster %s analyse error %s" %
+                      cluster_name, six.text_type(e))
+            raise e
+        return capacity
+
+    def get_cluster_used_capacity(self, cluster_name):
+        resposne_summary = self.rest_handler. \
+            get_virtual_volume_summary_resp(cluster_name)
+        try:
+            custom_data = resposne_summary.get("custom-data")
+            find_capacity = re.findall(
+                r"capacity\s+is\s+(([0-9]*(\.[0-9]{1,3}))|([0-9]+))",
+                custom_data)
+            find_capacity_str = find_capacity[-1][0]
+            find_capacity_float = float(find_capacity_str)
+            capacity = int(find_capacity_float * units.Ti)
+        except Exception as e:
+            LOG.error("Storage used capacity, cluster %s analyse error %s" %
+                      cluster_name, six.text_type(e))
+            raise e
+        return capacity
+
+
+@staticmethod
+def handle_detail_list(detail_info, detail_map, split):
+    detail_arr = detail_info.split('\n')
+    for detail in detail_arr:
+        if detail is not None and detail != '':
+            strinfo = detail.split(split, 1)
+            key = strinfo[0]
+            value = ''
+            if len(strinfo) > 1:
+                value = strinfo[1]
+            detail_map[key] = value
