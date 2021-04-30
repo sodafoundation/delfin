8abdde3b7b1f18329774fa5c423f718ff1493d75
Amit Roushan <amit.roushan@huawei.com>
2021-04-30 09:32:43 +0530

    fix Dockerfile and docker-compose for multi container deployment (#532)
    Co-authored-by: Erik <lynheell@gmail.com>
    Co-authored-by: Najmudheen <45681499+NajmudheenCT@users.noreply.github.com>

--- script/start.sh
+++ script/start.sh
@@ -13,15 +13,26 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+set -e
 
-python3 setup.py install
-
-python3 script/create_db.py --config-file /etc/delfin/delfin.conf
-
-sleep 10
-
-python3 delfin/cmd/api.py --config-file /etc/delfin/delfin.conf > /var/log/delfin/api.log 2>&1 &
-
-python3 delfin/cmd/task.py --config-file /etc/delfin/delfin.conf > /var/log/delfin/task.log 2>&1 &
-
-python3 delfin/cmd/alert.py --config-file /etc/delfin/delfin.conf > /var/log/delfin/alert.log 2>&1
+case "$1" in
+  api)
+    # Register database schema
+    python3 script/create_db.py --config-file /etc/delfin/delfin.conf
+    # Run API Server
+    exec python3 delfin/cmd/api.py --config-file /etc/delfin/delfin.conf
+    ;;
+  task)
+    exec python3 delfin/cmd/task.py --config-file /etc/delfin/delfin.conf
+    ;;
+  alert)
+    exec python3 delfin/cmd/alert.py --config-file /etc/delfin/delfin.conf
+    ;;
+  exporter)
+    exec python3 delfin/exporter/prometheus/exporter_server.py --config-file /etc/delfin/delfin.conf
+    ;;
+  *)
+    echo "Usage: $0 {api|task|alert|exporter}" >&2
+    exit 1
+    ;;
+esac
