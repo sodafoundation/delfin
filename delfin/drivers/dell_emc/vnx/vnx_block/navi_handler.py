7ec678486d3603864e241ec966b80899ec1f2729
yuanyu-ghca <79956159+yuanyu-ghca@users.noreply.github.com>
2021-05-18 17:34:37 +0800

    Emc vnx block do some optimize (#571)

--- delfin/drivers/dell_emc/vnx/vnx_block/navi_handler.py
+++ delfin/drivers/dell_emc/vnx/vnx_block/navi_handler.py
@@ -11,19 +11,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import re
 import threading
-import time
 
 import six
 from oslo_log import log as logging
-from oslo_utils import units
 
 from delfin import cryptor
 from delfin import exception
 from delfin.drivers.dell_emc.vnx.vnx_block import consts
 from delfin.drivers.dell_emc.vnx.vnx_block.navicli_client import NaviClient
-from delfin.drivers.utils.tools import Tools
 
 LOG = logging.getLogger(__name__)
 
@@ -44,13 +40,16 @@ class NaviHandler(object):
         self.verify = kwargs.get('verify', False)
         self.session_lock = threading.Lock()
 
-    def get_cli_command_str(self, host_ip=None, sub_command=None):
+    def get_cli_command_str(self, host_ip=None, sub_command=None,
+                            timeout=None):
         if host_ip is None:
             host_ip = self.navi_host
+        if timeout is None:
+            timeout = self.navi_timeout
         command_str = consts.NAVISECCLI_API % {
             'username': self.navi_username,
             'password': cryptor.decode(self.navi_password),
-            'host': host_ip, 'timeout': self.navi_timeout}
+            'host': host_ip, 'timeout': timeout}
         if self.navi_port:
             command_str = '%s -port %d' % (command_str, self.navi_port)
         command_str = '%s %s' % (command_str, sub_command)
@@ -71,7 +70,8 @@ class NaviHandler(object):
             NaviClient.exec(cer_add_command.split())
         command_str = \
             self.get_cli_command_str(host_ip=host_ip,
-                                     sub_command=consts.GET_AGENT_API)
+                                     sub_command=consts.GET_AGENT_API,
+                                     timeout=consts.LOGIN_SOCKET_TIMEOUT)
         result = NaviClient.exec(command_str.split(), stdin_value=accept_cer)
         if result:
             agent_model = self.cli_res_to_dict(result)
@@ -119,86 +119,19 @@ class NaviHandler(object):
         return self.get_resources_info(consts.GET_GETALLLUN_API,
                                        self.cli_lun_to_list)
 
-    def get_log(self, host_ip, query_para=None):
-        log_list = []
-        try:
-            query_time_list = self.get_log_query_time_list(query_para)
-            for i in range(len(query_time_list) - 1, -1, -1):
-                query_time = query_time_list[i]
-                command_str = consts.GET_LOG_API % {
-                    'begin_time': query_time.get('begin_time'),
-                    'end_time': query_time.get('end_time')}
-                command_str = self.get_cli_command_str(host_ip=host_ip,
-                                                       sub_command=command_str)
-                result = self.navi_exe(command_str.split(), host_ip)
-                if result:
-                    log_list.extend(self.cli_log_to_list(result))
-                else:
-                    break
-        except Exception as e:
-            # The log query error of a single control node does not affect
-            # the overall return, so only the log is recorded here,
-            # and no exception is thrown
-            err_msg = "Failed to get log from %s: %s" \
-                      % (host_ip, six.text_type(e))
-            LOG.error(err_msg)
-        return log_list
-
-    def get_log_query_time_list(self, query_para=None):
-        log_query_time_list = []
-        try:
-            if query_para is None:
-                tmp_begin = (time.time() -
-                             consts.SECS_OF_DEFAULT_QUERY_LOG_DAYS) * units.k
-                tmp_end = (time.time() + consts.ONE_DAY_SCE) * units.k
-                query_para = {
-                    'begin_time': str(tmp_begin),
-                    'end_time': str(tmp_end)
-                }
-            begin_time_int = int(float(query_para.get('begin_time')))
-            end_time_int = int(float(query_para.get('end_time')))
-            next_start_time = (begin_time_int +
-                               consts.SECS_OF_QUERY_TIME_RANGE_DAYS)
-            if next_start_time < end_time_int:
-                while (begin_time_int < end_time_int):
-                    begin_time_tmp = begin_time_int
-                    end_time_tmp = (begin_time_int +
-                                    consts.SECS_OF_QUERY_TIME_RANGE_DAYS)
-                    if end_time_tmp > end_time_int:
-                        end_time_tmp = end_time_int
-                    query_time = self.log_query_time_to_list(begin_time_tmp,
-                                                             end_time_tmp)
-                    log_query_time_list.append(query_time)
-                    begin_time_int = end_time_tmp
-            else:
-                query_time = self.log_query_time_to_list(begin_time_int,
-                                                         end_time_int)
-                log_query_time_list.append(query_time)
-        except Exception as e:
-            err_msg = "Error splitting query time: %s" % (six.text_type(e))
-            LOG.error(err_msg)
-            raise exception.InvalidResults(err_msg)
-        return log_query_time_list
-
-    def log_query_time_to_list(self, begin_timestamp, end_timestamp):
-        tools = Tools()
-        begin_time = tools.timestamp_to_time_str(begin_timestamp,
-                                                 consts.DATE_PATTERN)
-        end_time = tools.timestamp_to_time_str(end_timestamp,
-                                               consts.DATE_PATTERN)
-        query_time_map = {
-            'begin_time': begin_time,
-            'end_time': end_time
-        }
-        return query_time_map
-
     def get_resources_info(self, sub_command, analyse_type):
-        """Execute commands to query data and analyze"""
-        command_str = self.get_cli_command_str(sub_command=sub_command)
-        resource_info = self.navi_exe(command_str.split())
-        return_value = None
-        if resource_info:
-            return_value = analyse_type(resource_info)
+        # Execute commands to query data and analyze
+        try:
+            command_str = self.get_cli_command_str(sub_command=sub_command)
+            resource_info = self.navi_exe(command_str.split())
+            return_value = None
+            if resource_info:
+                return_value = analyse_type(resource_info)
+        except Exception as e:
+            err_msg = "Failed to get resources info from %s: %s" \
+                      % (sub_command, six.text_type(e))
+            LOG.error(err_msg)
+            raise e
         return return_value
 
     def cli_res_to_dict(self, resource_info):
@@ -333,39 +266,6 @@ class NaviHandler(object):
             raise exception.InvalidResults(err_msg)
         return obj_list
 
-    def cli_log_to_list(self, resource_info):
-        obj_list = []
-        try:
-            tools = Tools()
-            # Filter log information for log codes 70-77
-            pattern = re.compile(consts.LOG_FILTER_PATTERN)
-            obj_infos = resource_info.split('\n')
-            for obj_info in obj_infos:
-                str_line = obj_info.strip()
-                if str_line:
-                    search_obj = pattern.search(str_line)
-                    if search_obj:
-                        str_line = str_line.replace(
-                            'See alerts for details.', '')
-                        str_infos = str_line.split(search_obj.group())
-                        str_0 = str_infos[0].strip()
-                        log_time = str_0[0:str_0.rindex(' ')]
-                        event_code = search_obj.group() \
-                            .replace('(', '').replace(')', '')
-                        obj_model = {
-                            'log_time': log_time,
-                            'log_time_stamp': tools.time_str_to_timestamp(
-                                log_time, consts.TIME_PATTERN),
-                            'event_code': event_code,
-                            'message': str_infos[1].strip()
-                        }
-                        obj_list.append(obj_model)
-        except Exception as e:
-            err_msg = "arrange log info error: %s", six.text_type(e)
-            LOG.error(err_msg)
-            raise exception.InvalidResults(err_msg)
-        return obj_list
-
     def analyse_cer(self, resource_info, host_ip=None):
         cer_map = {}
         obj_model = {}
@@ -443,5 +343,9 @@ class NaviHandler(object):
             self.login(host_ip)
             result = NaviClient.exec(command_str)
             return result
+        except Exception as e:
+            err_msg = "naviseccli exec error: %s" % (six.text_type(e))
+            LOG.error(err_msg)
+            raise e
         finally:
             self.session_lock.release()
