e6d84d6a9d957c2096dcf57b116db7e707725edf
guankc <kecheng.guan@gh-ca.com>
2021-05-20 16:57:40 +0800

    2020.5.20 commit

--- delfin/drivers/netapp/dataontap/constants.py
+++ delfin/drivers/netapp/dataontap/constants.py
@@ -43,7 +43,7 @@ DISK_SPLIT_STR = "     Dis"
 LUN_SHOW_DETAIL_COMMAND = "lun show -instance"
 LUN_SPLIT_STR = "  Vserver"
 
-STORAGE_VENDOR = "netappp"
+STORAGE_VENDOR = "netapp"
 STORAGE_MODEL = "cmodel"
 
 STORAGE_STATUS = {
