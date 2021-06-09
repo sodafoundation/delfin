joseph-v/dolphin

Add centralized manager support to delfin

delfin/db/sqlalchemy/api.py
@@ -316,6 +316,96 @@ def storage_delete(context, storage_id):
     _storage_get_query(context).filter_by(id=storage_id).update(delete_info)
 
 
+def centralized_manager_create(context, values):
+    """Add a centralized_manager device from the values dictionary."""
+    if not values.get('driver_id'):
+        values['driver_id'] = uuidutils.generate_uuid()
+
+    centralized_manager_ref = models.CentralizedManager()
+    centralized_manager_ref.update(values)
+
+    session = get_session()
+    with session.begin():
+        session.add(centralized_manager_ref)
+
+    return _centralized_manager_get(context,
+                                    centralized_manager_ref['driver_id'],
+                                    session=session)
+
+
+def centralized_manager_update(context, centralized_manager_id, values):
+    """Update a centralized_manager device with the values dictionary."""
+    session = get_session()
+    with session.begin():
+        query = _centralized_manager_get_query(context, session)
+        result = query.filter_by(
+            driver_id=centralized_manager_id).update(values)
+    return result
+
+
+def centralized_manager_get(context, centralized_manager_id):
+    """Retrieve a centralized_manager device."""
+    return _centralized_manager_get(context, centralized_manager_id)
+
+
+def _centralized_manager_get(context, centralized_manager_id, session=None):
+    result = (_centralized_manager_get_query(context, session=session)
+              .filter_by(driver_id=centralized_manager_id)
+              .first())
+
+    if not result:
+        raise exception.CentralizedManagerNotFound(centralized_manager_id)
+
+    return result
+
+
+def _centralized_manager_get_query(context, session=None):
+    read_deleted = context.read_deleted
+    kwargs = dict()
+
+    if read_deleted in ('no', 'n', False):
+        kwargs['deleted'] = False
+    elif read_deleted in ('yes', 'y', True):
+        kwargs['deleted'] = True
+
+    return model_query(context, models.CentralizedManager,
+                       session=session, **kwargs)
+
+
+def centralized_manager_get_all(context, marker=None,
+                                limit=None, sort_keys=None,
+                                sort_dirs=None, filters=None, offset=None):
+    session = get_session()
+    with session.begin():
+        # Generate the query
+        query = _generate_paginate_query(context, session,
+                                         models.CentralizedManager,
+                                         marker, limit, sort_keys, sort_dirs,
+                                         filters, offset,
+                                         )
+        # No centralized_managers   match, return empty list
+        if query is None:
+            return []
+        return query.all()
+
+
+@apply_like_filters(model=models.CentralizedManager)
+def _process_centralized_manager_info_filters(query, filters):
+    """Common filter processing for CentralizedManagers queries."""
+    if filters:
+        if not is_valid_model_filters(models.CentralizedManager, filters):
+            return
+        query = query.filter_by(**filters)
+    return query
+
+
+def centralized_manager_delete(context, centralized_manager_id):
+    """Delete a centralized_manager device."""
+    delete_info = {'deleted': True, 'deleted_at': timeutils.utcnow()}
+    _centralized_manager_get_query(context).filter_by(
+        driver_id=centralized_manager_id).update(delete_info)
+
+
 def _volume_get_query(context, session=None):
     return model_query(context, models.Volume, session=session)
 
@@ -1859,6 +1949,9 @@ PAGINATION_HELPERS = {
                          _storage_pool_get),
     models.Storage: (_storage_get_query, _process_storage_info_filters,
                      _storage_get),
+    models.CentralizedManager: (_centralized_manager_get_query,
+                                _process_centralized_manager_info_filters,
+                                _centralized_manager_get),
     models.AlertSource: (_alert_source_get_query,
                          _process_alert_source_filters,
                          _alert_source_get),

delfin/drivers/api.py
@@ -49,6 +49,41 @@ class API(object):
         LOG.info("Storage found successfully.")
         return storage
 
+    def discover_storages(self, context, access_info):
+        """Discover a centralized_manager system with access information."""
+        print("------CM-------: In discover cm")
+        helper.encrypt_password(context, access_info)
+        if 'driver_id' not in access_info:
+            access_info['driver_id'] = six.text_type(
+                uuidutils.generate_uuid())
+
+        driver = self.driver_manager.get_driver(context,
+                                                cache_on_load=False,
+                                                **access_info)
+        storages = driver.get_storages(context)
+
+        # print ("---JVP-------, storages", storages)
+
+        # Need to validate storages response from driver
+        storages_list = []
+        for storage in storages:
+            helper.check_storage_repetition(context, storage)
+            storage['id'] = six.text_type(
+                uuidutils.generate_uuid())
+            access_info['storage_id'] = storage['id']
+            db.access_info_create(context, access_info)
+            storage_ret = db.storage_create(context, storage)
+            storages_list.append(storage_ret)
+        LOG.info("Storages from Centralized Mgr found successfully.")
+        cm = {
+            "driver_id": access_info['driver_id'],
+            "resources": {
+                "storages": storages
+            }
+        }
+        cm = db.centralized_manager_create(context, cm)
+        return cm
+
     def update_access_info(self, context, access_info):
         """Validate and update access information."""
         helper.encrypt_password(context, access_info)
@@ -68,7 +103,7 @@ class API(object):
 
     def remove_storage(self, context, storage_id):
         """Clear driver instance from driver factory."""
-        self.driver_manager.remove_driver(storage_id)
+        self.driver_manager.remove_driver(context, storage_id)
 
     def get_storage(self, context, storage_id):
         """Get storage device information from storage system"""
@@ -78,49 +113,49 @@ class API(object):
     def list_storage_pools(self, context, storage_id):
         """List all storage pools from storage system."""
         driver = self.driver_manager.get_driver(context, storage_id=storage_id)
-        return driver.list_storage_pools(context)
+        return driver.list_storage_pools(context, storage_id)
 
     def list_volumes(self, context, storage_id):
         """List all storage volumes from storage system."""
         driver = self.driver_manager.get_driver(context, storage_id=storage_id)
-        return driver.list_volumes(context)
+        return driver.list_volumes(context, storage_id)
 
     def list_controllers(self, context, storage_id):
         """List all storage controllers from storage system."""
 
         driver = self.driver_manager.get_driver(context, storage_id=storage_id)
-        return driver.list_controllers(context)
+        return driver.list_controllers(context, storage_id)
 
     def list_ports(self, context, storage_id):
         """List all ports from storage system."""
 
         driver = self.driver_manager.get_driver(context, storage_id=storage_id)
-        return driver.list_ports(context)
+        return driver.list_ports(context, storage_id)
 
     def list_disks(self, context, storage_id):
         """List all disks from storage system."""
         driver = self.driver_manager.get_driver(context, storage_id=storage_id)
-        return driver.list_disks(context)
+        return driver.list_disks(context, storage_id)
 
     def list_quotas(self, context, storage_id):
         """List all quotas from storage system."""
         driver = self.driver_manager.get_driver(context, storage_id=storage_id)
-        return driver.list_quotas(context)
+        return driver.list_quotas(context, storage_id)
 
     def list_filesystems(self, context, storage_id):
         """List all filesystems from storage system."""
         driver = self.driver_manager.get_driver(context, storage_id=storage_id)
-        return driver.list_filesystems(context)
+        return driver.list_filesystems(context, storage_id)
 
     def list_qtrees(self, context, storage_id):
         """List all qtrees from storage system."""
         driver = self.driver_manager.get_driver(context, storage_id=storage_id)
-        return driver.list_qtrees(context)
+        return driver.list_qtrees(context, storage_id)
 
     def list_shares(self, context, storage_id):
         """List all shares from storage system."""
         driver = self.driver_manager.get_driver(context, storage_id=storage_id)
-        return driver.list_shares(context)
+        return driver.list_shares(context, storage_id)
 
     def add_trap_config(self, context, storage_id, trap_config):
         """Config the trap receiver in storage system."""
@@ -161,4 +196,4 @@ class API(object):
     def get_capabilities(self, context, storage_id,):
         """Get capabilities from supported driver"""
         driver = self.driver_manager.get_driver(context, storage_id=storage_id)
-        return driver.get_capabilities(context)
+        return driver.get_capabilities(context, storage_id)

sodafoundation/delfin

delfin/db/sqlalchemy/api.py
@@ -316,6 +316,96 @@ def storage_delete(context, storage_id):
     _storage_get_query(context).filter_by(id=storage_id).update(delete_info)
 
 
+def centralized_manager_create(context, values):
+    """Add a centralized_manager device from the values dictionary."""
+    if not values.get('id'):
+        values['id'] = uuidutils.generate_uuid()
+
+    centralized_manager_ref = models.CentralizedManager()
+    centralized_manager_ref.update(values)
+
+    session = get_session()
+    with session.begin():
+        session.add(centralized_manager_ref)
+
+    return _centralized_manager_get(context,
+                                    centralized_manager_ref['id'],
+                                    session=session)
+
+
+def centralized_manager_update(context, centralized_manager_id, values):
+    """Update a centralized_manager device with the values dictionary."""
+    session = get_session()
+    with session.begin():
+        query = _centralized_manager_get_query(context, session)
+        result = query.filter_by(
+            id=centralized_manager_id).update(values)
+    return result
+
+
+def centralized_manager_get(context, centralized_manager_id):
+    """Retrieve a centralized_manager device."""
+    return _centralized_manager_get(context, centralized_manager_id)
+
+
+def _centralized_manager_get(context, centralized_manager_id, session=None):
+    result = (_centralized_manager_get_query(context, session=session)
+              .filter_by(id=centralized_manager_id)
+              .first())
+
+    if not result:
+        raise exception.CentralizedManagerNotFound(centralized_manager_id)
+
+    return result
+
+
+def _centralized_manager_get_query(context, session=None):
+    read_deleted = context.read_deleted
+    kwargs = dict()
+
+    if read_deleted in ('no', 'n', False):
+        kwargs['deleted'] = False
+    elif read_deleted in ('yes', 'y', True):
+        kwargs['deleted'] = True
+
+    return model_query(context, models.CentralizedManager,
+                       session=session, **kwargs)
+
+
+def centralized_manager_get_all(context, marker=None,
+                                limit=None, sort_keys=None,
+                                sort_dirs=None, filters=None, offset=None):
+    session = get_session()
+    with session.begin():
+        # Generate the query
+        query = _generate_paginate_query(context, session,
+                                         models.CentralizedManager,
+                                         marker, limit, sort_keys, sort_dirs,
+                                         filters, offset,
+                                         )
+        # No centralized_managers   match, return empty list
+        if query is None:
+            return []
+        return query.all()
+
+
+@apply_like_filters(model=models.CentralizedManager)
+def _process_centralized_manager_info_filters(query, filters):
+    """Common filter processing for CentralizedManagers queries."""
+    if filters:
+        if not is_valid_model_filters(models.CentralizedManager, filters):
+            return
+        query = query.filter_by(**filters)
+    return query
+
+
+def centralized_manager_delete(context, centralized_manager_id):
+    """Delete a centralized_manager device."""
+    delete_info = {'deleted': True, 'deleted_at': timeutils.utcnow()}
+    _centralized_manager_get_query(context).filter_by(
+        id=centralized_manager_id).update(delete_info)
+
+
 def _volume_get_query(context, session=None):
     return model_query(context, models.Volume, session=session)
 
@@ -1859,6 +1949,9 @@ PAGINATION_HELPERS = {
                          _storage_pool_get),
     models.Storage: (_storage_get_query, _process_storage_info_filters,
                      _storage_get),
+    models.CentralizedManager: (_centralized_manager_get_query,
+                                _process_centralized_manager_info_filters,
+                                _centralized_manager_get),
     models.AlertSource: (_alert_source_get_query,
                          _process_alert_source_filters,
                          _alert_source_get),

delfin/drivers/api.py
@@ -49,6 +49,33 @@ class API(object):
         LOG.info("Storage found successfully.")
         return storage
 
+    def discover_centralized_manager(self, context, access_info):
+        """Discover a centralized_manager system with access information."""
+        helper.encrypt_password(context, access_info)
+        if 'storage_id' not in access_info:
+            access_info['storage_id'] = six.text_type(
+                uuidutils.generate_uuid())
+        driver = self.driver_manager.get_driver(context,
+                                                cache_on_load=False,
+                                                **access_info)
+        cm = driver.get_centralized_manager(context)
+        # Need to validate centralized_manager response from driver
+        helper.check_cm_repetition(context, cm)
+        db.access_info_create(context, access_info)
+        cm['id'] = access_info['storage_id']
+
+        for storage in cm['storages']:
+            helper.check_storage_repetition(context, storage)
+            storage['id'] = six.text_type(
+                uuidutils.generate_uuid())
+            access_info['storage_id'] = storage['id']
+            db.access_info_create(context, access_info)
+            db.storage_create(context, storage)
+
+        cm = db.centralized_manager_create(context, cm)
+        LOG.info("Storages from Centralized Mgr found successfully.")
+        return cm
+
     def update_access_info(self, context, access_info):
         """Validate and update access information."""
         helper.encrypt_password(context, access_info)
@@ -66,6 +93,23 @@ class API(object):
         LOG.info("Access information updated successfully.")
         return access_info
 
+    def update_cm_access_info(self, context, access_info):
+        """Validate and update access information."""
+        helper.encrypt_password(context, access_info)
+        driver = self.driver_manager.get_driver(context,
+                                                cache_on_load=False,
+                                                **access_info)
+        cm_new = driver.get_centralized_manager(context)
+
+        # Need to validate storage response from driver
+        storage_id = access_info['storage_id']
+        helper.check_cm_consistency(context, storage_id, cm_new)
+        access_info = db.access_info_update(context, storage_id, access_info)
+        db.centralized_manager_update(context, storage_id, cm_new)
+
+        LOG.info("Access information updated successfully.")
+        return access_info
+
     def remove_storage(self, context, storage_id):
         """Clear driver instance from driver factory."""
         self.driver_manager.remove_driver(storage_id)
@@ -74,6 +118,11 @@ class API(object):
         """Get storage device information from storage system"""
         driver = self.driver_manager.get_driver(context, storage_id=storage_id)
         return driver.get_storage(context)
+
+    def get_centralized_manager(self, context, cm_id):
+        """Get storage device information from storage system"""
+        driver = self.driver_manager.get_driver(context, storage_id=cm_id)
+        return driver.get_centralized_manager(context)
 
     def list_storage_pools(self, context, storage_id):
         """List all storage pools from storage system."""