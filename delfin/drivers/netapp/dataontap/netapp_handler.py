3d5f07830743e1979979f71faf2834f8a29397cb
guankc <80501304+guankc@users.noreply.github.com>
2021-07-13 10:12:31 +0800

    Update netapp_handler.py

--- delfin/drivers/netapp/dataontap/netapp_handler.py
+++ delfin/drivers/netapp/dataontap/netapp_handler.py
@@ -12,7 +12,6 @@
 # WarrayANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-
 import time
 import six
 import hashlib
