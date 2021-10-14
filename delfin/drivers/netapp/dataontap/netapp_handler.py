0adcdf67b12d0728a38a140a1252067fa1a031b5
guankc <80501304+guankc@users.noreply.github.com>
2021-10-14 14:13:54 +0800

    Update netapp_handler.py

--- delfin/drivers/netapp/dataontap/netapp_handler.py
+++ delfin/drivers/netapp/dataontap/netapp_handler.py
@@ -41,7 +41,7 @@ class NetAppHandler(object):
     OID_TRAP_DATA = '1.3.6.1.4.1.789.1.1.12.0'
     NODE_NAME = 'controller_name'
     SECONDS_TO_MS = 1000
-    NETAPP_NAA = 'NAA.60a98000'
+    NETAPP_NAA = '60a98000'
 
     def __init__(self, **kwargs):
         self.ssh_pool = SSHPool(**kwargs)
