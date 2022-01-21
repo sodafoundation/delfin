48dd99a413c8ac5bc75f456331990e5b8adc43ee
yuanyu <yu.yuan@gh-ca.com>
2022-01-21 15:09:11 +0800

    Handling view return issues

--- delfin/tests/unit/drivers/hpe/hpe_3par/test_hpe_3parstor.py
+++ delfin/tests/unit/drivers/hpe/hpe_3par/test_hpe_3parstor.py
@@ -1329,7 +1329,6 @@ VIEW_RESULT = [
     {
         'native_masking_view_id': '2_0:2:1',
         'name': '2',
-        'native_port_group_id': 'port_group_0:2:1',
         'storage_id': '12345',
         'native_volume_id': '666',
         'native_storage_host_id': '160'
