877f30ed3ddf785442d0517767e49405930664ec
guankc <80501304+guankc@users.noreply.github.com>
2022-04-24 19:41:00 +0800

    Update netapp_handler.py

--- delfin/drivers/netapp/dataontap/netapp_handler.py
+++ delfin/drivers/netapp/dataontap/netapp_handler.py
@@ -1101,7 +1101,7 @@ class NetAppHandler(object):
                 get_perf_value(metrics, storage_id,
                                start_time, end_time,
                                json_info,
-                               storage['ClusterUUID'] + '_' 
+                               storage['ClusterUUID'] + '_'
                                + storage['ClusterSerialNumber'],
                                storage['ClusterName'],
                                constants.ResourceType.STORAGE)
