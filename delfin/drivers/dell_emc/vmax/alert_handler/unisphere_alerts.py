e3aa3514eaabccc814b27d44f65e350f5033a5c0
Arjun <arjungkanikeri@gmail.com>
2022-04-28 06:14:17 +0530

    Removed _init_ function

--- delfin/drivers/dell_emc/vmax/alert_handler/unisphere_alerts.py
+++ delfin/drivers/dell_emc/vmax/alert_handler/unisphere_alerts.py
@@ -34,8 +34,7 @@ class AlertHandler(object):
                     "NORMAL": constants.Severity.INFORMATIONAL,
                     "INFORMATION": constants.Severity.INFORMATIONAL}
 
-    def __init__(self):
-        pass
+   
 
     def parse_queried_alerts(self, alert_list):
         """Parse queried alerts and convert to alert model."""
