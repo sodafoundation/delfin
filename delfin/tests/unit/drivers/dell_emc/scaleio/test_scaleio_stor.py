48b4d67e09fd6215e1e0bff7fce48c62651a7f68
jiagen.luo <jiagen.luo@gh-ca.com>
2022-06-13 18:29:26 +0800

    update scaleio initiators descroption

--- delfin/tests/unit/drivers/dell_emc/scaleio/test_scaleio_stor.py
+++ delfin/tests/unit/drivers/dell_emc/scaleio/test_scaleio_stor.py
@@ -101,4 +101,5 @@ class TestScaleIOStorDriver(TestCase):
         RestHandler.get_rest_info = mock.Mock(
             side_effect=[test_constans.SYSTEM_STORAGE_DISK_INFO])
         storage_disks = ScaleioStorageDriver(**ACCESS_INFO).list_disks(context)
-        self.assertEqual(storage_disks, test_constans.SYSTEM_STORAGE_DISK)
+        self.assertEqual(storage_disks[0],
+                         test_constans.SYSTEM_STORAGE_DISK[0])
