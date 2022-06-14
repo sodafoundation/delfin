3ca09285c094ee284a837c5342a38e7749af5d3d
jiagen.luo <jiagen.luo@gh-ca.com>
2022-06-14 14:53:51 +0800

    update scaleio test_list_disks interface

--- delfin/tests/unit/drivers/dell_emc/scaleio/test_scaleio_stor.py
+++ delfin/tests/unit/drivers/dell_emc/scaleio/test_scaleio_stor.py
@@ -109,5 +109,6 @@ class TestScaleIOStorDriver(TestCase):
     @mock.patch.object(RestHandler, 'get_rest_info')
     def test_list_disks(self, mock_disks):
         mock_disks.return_value = test_constans.SYSTEM_STORAGE_DISK_INFO
-        storage_disks = ScaleioStorageDriver(**ACCESS_INFO).list_disks(context)
+        storage_id = '12345'
+        storage_disks = RestHandler(**ACCESS_INFO).list_disks(storage_id)
         self.assertEqual(storage_disks, test_constans.SYSTEM_STORAGE_DISK)
