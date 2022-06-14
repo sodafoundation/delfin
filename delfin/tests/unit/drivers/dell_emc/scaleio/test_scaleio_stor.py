c4d71b6fd9df14bee9fe99ec8a67ae434be0a8fd
jiagen.luo <jiagen.luo@gh-ca.com>
2022-06-14 11:21:14 +0800

    update scaleio test_list_disk descroption

--- delfin/tests/unit/drivers/dell_emc/scaleio/test_scaleio_stor.py
+++ delfin/tests/unit/drivers/dell_emc/scaleio/test_scaleio_stor.py
@@ -97,14 +97,6 @@ class TestScaleIOStorDriver(TestCase):
             .list_storage_hosts(context)
         self.assertEqual(storage_host, test_constans.SYSTEM_HOST)
 
-    def test_list_disks(self):
-        RestHandler.get_rest_info = mock.Mock(side_effect=[
-            test_constans.SYSTEM_STORAGE_DISK_INFO,
-            test_constans.SYSTEM_DISK_DETAIL])
-        storage_disks = ScaleioStorageDriver(**ACCESS_INFO) \
-            .list_disks(context)
-        self.assertEqual(storage_disks, test_constans.SYSTEM_STORAGE_DISK)
-
     def test_parse_alert(self):
         trap_alert = test_constans.SYSTEM_TRAP_ALERT
         storage_trap_alert = ScaleioStorageDriver(**ACCESS_INFO).\
