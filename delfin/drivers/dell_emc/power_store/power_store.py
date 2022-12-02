95e53b30febf268b14dab80ddcd0cfb404e38719
Administrator <zhilong.xu@gh-ca.com>
2022-12-02 18:55:22 +0800

    Code review optimization

--- delfin/drivers/dell_emc/power_store/power_store.py
+++ delfin/drivers/dell_emc/power_store/power_store.py
@@ -1,3 +1,16 @@
+# Copyright 2022 The SODA Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 from delfin.common import constants
 from delfin.drivers import driver
 from oslo_log import log
@@ -149,15 +162,3 @@ class PowerStoreDriver(driver.StorageDriver):
 
     def list_masking_views(self, context):
         return self.rest_handler.list_masking_views(self.storage_id)
-
-    def list_quotas(self, context):
-        return []
-
-    def list_filesystems(self, context):
-        return []
-
-    def list_qtrees(self, context):
-        return []
-
-    def list_shares(self, context):
-        return []
