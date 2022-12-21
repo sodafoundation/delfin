628ed3bd439d97da83f8d8db1d9b999e657bfe93
guankc <80501304+guankc@users.noreply.github.com>
2022-12-21 11:02:25 +0800

    Update netapp_handler.py

--- delfin/drivers/netapp/dataontap/netapp_handler.py
+++ delfin/drivers/netapp/dataontap/netapp_handler.py
@@ -531,6 +531,9 @@ class NetAppHandler(object):
             ips_array = self.get_table_data(controller_ips)
             ip_map = {}
             controller_map_list = []
+            pattern = constant.IP_PATTERN if \
+                constant.IP_PATTERN.search(self.ssh_pool.ssh_host) else \
+                constant.IP_V6_PATTERN
             Tools.split_value_map_list(
                 controller_info, controller_map_list, split=':')
             for controller_map in controller_map_list:
@@ -542,16 +545,10 @@ class NetAppHandler(object):
                             for ip in ip_array:
                                 if ip == controller_map['Node']:
                                     key = ip
-                                    continue
-                                if constant.IP_PATTERN. \
-                                        search(self.ssh_pool.ssh_host):
-                                    if constant.IP_PATTERN.search(ip) and key:
-                                        value = ip
-                                        ip_map[key] = value
-                                elif constant.IP_V6_PATTERN.search(ip) \
-                                        and key:
+                                if pattern.search(ip):
                                     value = ip
-                                    ip_map[key] = value
+                            if value and key:
+                                ip_map[key] = value
                     status = constants.ControllerStatus.NORMAL \
                         if controller_map['Health'] == 'true' \
                         else constants.ControllerStatus.OFFLINE
